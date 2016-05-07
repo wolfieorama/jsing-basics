e9c1c3145f26c6396647c4493fdf5d5d9757f7bd
wolfieorama <wachirawamunyi@gmail.com>
2016-05-07 11:22:46 +0300

    fixed  charAt passing

--- countE.js
+++ countE.js
@@ -1,14 +1,14 @@
-function countE(){
+function countE (){
   var phrase = prompt("Enter a something sir here sir!!");
 
-  if(typeof(phrase) != "string"){
+  if (typeof(phrase) != "string"){
     alert("What you Entered is not a string type!");
     return false;
   }
   else{
     var eCount = 0;
     for(var index = 0; index < phrase.length; index++){
-      if(phrase.charAt(index) == "e" || phrase.charAt(index == "E"))
+      if(phrase.charAt(index) == 'e' || phrase.charAt(index) == 'E')
         eCount++;
     }
     alert("there are " +eCount+ " Es in your phrase!");
