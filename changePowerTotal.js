360ad3241a096a4b92414f99b44bb50c3f258183
wolfieorama <wachirawamunyi@gmail.com>
2016-05-07 12:44:35 +0300

    added ChangePowerTotal fn

---
+++ changePowerTotal.js
@@ -0,0 +1,12 @@
+function changePowerTotal (totalPower, genID, genStatus, amtOfPower){
+  for(var index = 0; index < 19; index++){
+    if (genStatus == "on"){
+      alert("Generator #" +genID+  " is now on, adding " + amtOfPower + " MW, for a total of " +(totalPower + amtOfPower)+ " MW!");
+      return totalPower + amtOfPower;
+    }
+    else if (genStatus == "off"){
+      alert("Generator #" +genID+  " is now on, adding " + amtOfPower + " MW, for a total of " +(totalPower - amtOfPower)+ " MW!");
+       return totalPower - amtOfPower;
+    }
+  }
+}
