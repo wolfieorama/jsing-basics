db9cffa10eea8847cee76a9192dfa96d5f0414a1
wolfieorama <wachirawamunyi@gmail.com>
2016-05-18 14:24:40 +0300

    fn expressions vs declared fns

---
+++ fearGenerated.js
@@ -0,0 +1,12 @@
+var people = 5;
+var rain = 20;
+var sharks = 5;
+
+var fearGenerated = function(numPeeps, rainInInches, numSharks) {
+  var rainFear = numPeeps * rainInInches;
+  var sharkFear = numSharks * numSharks * numSharks;
+  var totalFear = sharkFear + rainFear;
+  return totalFear;
+};
+
+var fear = fearGenerated(people, rain, sharks);
