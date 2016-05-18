c748846cb243d581ec5beccc6a4a42dd32dbe09c
wolfieorama <wachirawamunyi@gmail.com>
2016-05-18 16:08:57 +0300

    using .map on arrays

---
+++ passingArraysMap.js
@@ -0,0 +1,7 @@
+var modifiedNames = [ "Thomas Meeks",
+                      "Gregg Pollack",
+                      "Christine Wong",
+                      "Dan McGaw" ];
+modifiedNames.map(function(arrayCell){
+  alert("Yo, " +arrayCell);
+});
