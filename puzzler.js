906b652729ebc3d4d3c10c75408d33a88a53d845
wolfieorama <wachirawamunyi@gmail.com>
2016-05-18 16:09:56 +0300

    puzzler array of fns

---
+++ puzzler.js
@@ -0,0 +1,14 @@
+var puzzlers = [
+  function(input){
+    return (3*input)-8;
+  },
+  function(input){
+    return (input + 2) * (input + 2) * (input + 2);
+  },
+  function(input){
+    return (input *input) - 9;
+  },
+  function(input){
+    return input % 4;
+  }
+];
