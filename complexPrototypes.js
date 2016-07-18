f1afd54e1fcc3f741ce83d06d602ef91f3540925
wolfieorama <wachirawamunyi@gmail.com>
2016-07-18 20:29:56 +0300

    inheritance in Objects and Arrays

---
+++ complexPrototypes.js
@@ -0,0 +1,28 @@
+var forestCows = [
+  {name: "Legolas", type: "calf", hadCalf: null},
+  {name: "Gimli", type: "bull", hadCalf: null},
+  {name: "Arwen", type: "cow", hadCalf: null},
+  {name: "Galadriel", type: "cow", hadCalf: null},
+  {name: "Eowyn", type: "cow", hadCalf: "Legolas"}
+];
+
+Object.prototype.noCalvesYet = function() {
+  if (this.type == "cow" && this.hadCalf == null) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+Array.prototype.countForBreeding = function() {
+  var numToBreed = 0;
+  for (var i = 0; i < this.length; i++) {
+    if (this[i].noCalvesYet() == true) { // don't need to explicitly compare to true
+      numToBreed++;
+    }
+  }
+  return numToBreed;
+};
+
+console.log(forestCows.countForBreeding());
